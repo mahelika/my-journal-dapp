f6f2fbde3d3e437f2b6c6f7200b1a90d6ad5ed82
mahelikaa <imahelika@gmail.com>
2025-04-30 22:59:25 +0530

    todo: connect to ui

--- anchor/programs/counter/src/lib.rs
+++ anchor/programs/counter/src/lib.rs
@@ -23,6 +23,26 @@ pub mod journal {
     journal_entry.message = message;
     Ok(())
   }
+
+  pub fn update_journal_entry(
+    ctx: Context<UpdateEntry>,
+    title: String,
+    message: String,
+  ) -> Result<()> {
+    msg!("Journal Entry Updated");
+    msg!("Title: {}", title);
+    msg!("Message: {}", message);
+
+    let journal_entry = &mut ctx.accounts.journal_entry;
+    journal_entry.message = message;
+
+    Ok(())
+  }
+
+  pub fn delete_journal_entry(_ctx: Context<DeleteEntry>, title: String) -> Result<()> {
+    msg!("Journal entry titled {} deleted", title);
+    Ok(())
+  }
 }
 
 #[account]
@@ -33,4 +53,52 @@ pub struct JournalEntryState {
   pub title: String,
   #[max_len(1000)]
   pub message: String
+}
+
+#[derive(Accounts)]
+#[instruction(title: String, message: String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init_if_needed,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        payer = owner,
+        space = 8 + JournalEntryState::INIT_SPACE
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String, message: String)]
+pub struct UpdateEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 1 + 4 + title.len() + 4 + message.len(),
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
