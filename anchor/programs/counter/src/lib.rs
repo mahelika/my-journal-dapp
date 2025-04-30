65fefc58251ef8f74e23694d4c412620f0a043cb
mahelikaa <imahelika@gmail.com>
2025-04-30 15:43:13 +0530

    journal entry

--- anchor/programs/counter/src/lib.rs
+++ anchor/programs/counter/src/lib.rs
@@ -5,66 +5,32 @@ use anchor_lang::prelude::*;
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod counter {
-    use super::*;
+pub mod journal {
+  use super::*;
 
-  pub fn close(_ctx: Context<CloseCounter>) -> Result<()> {
+  pub fn create_journal_entry(
+    ctx: Context<CreateEntry>,
+    title: String,
+    message: String,
+  ) -> Result<()> {
+    msg!("Journal Entry Created");
+    msg!("Title: {}", title);
+    msg!("Message: {}", message);
+
+    let journal_entry = &mut ctx.accounts.journal_entry;
+    journal_entry.owner = ctx.accounts.owner.key();
+    journal_entry.title = title;
+    journal_entry.message = message;
     Ok(())
   }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.counter.count = ctx.accounts.counter.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCounter>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.counter.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeCounter<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Counter::INIT_SPACE,
-  payer = payer
-  )]
-  pub counter: Account<'info, Counter>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCounter<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub counter: Account<'info, Counter>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub counter: Account<'info, Counter>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Counter {
-  count: u8,
+pub struct JournalEntryState {
+  pub owner: Pubkey,
+  #[max_len(50)]
+  pub title: String,
+  #[max_len(1000)]
+  pub message: String
 }
